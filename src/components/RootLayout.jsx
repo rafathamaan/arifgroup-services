5fee0571e752f6d017228ecca5fd3b9779ba53e2
RafathAmaan Mohammad <116374643+rafathamaan@users.noreply.github.com>
2024-08-31 20:20:46 +0530

    Update RootLayout.jsx

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -32,7 +32,7 @@ const Header = ({
         </Link>
         <div className="flex items-center gap-x-8">
         
-          <Button className={"bg-neutral-950"} href={"/contact"} invert={invert}>
+          <Button className={"bg-neutral-950 "} href={"/contact"} invert={invert}>
             Contact us
           </Button>
           <button
@@ -63,9 +63,9 @@ const Header = ({
 };
 const NavigationRow = ({ children }) => {
   return (
-    <div className="even:mt-px sm:bg-neutral-950">
+    <div className="even:mt-px sm:bg-neutral-950 ">
       <Container>
-        <div className="grid grid-cols-1 sm:grid-cols-2">{children}</div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 ">{children}</div>
       </Container>
     </div>
   );
@@ -75,17 +75,17 @@ const NavigationItem = ({ href, children }) => {
   return (
     <Link
       href={href}
-      className="group relative isolate -mx-6 bg-neutral-950 px-6 py-10 even:mt-px sm:mx-0 sm:px-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-l sm:even:border-neutral-800 sm:even:pl-16"
+      className="group relative isolate -mx-6 bg-neutral-950 px-6 py-10 even:mt-px sm:mx-0 sm:px-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-l sm:even:border-neutral-800 sm:even:pl-16 hover:text-blue-500 "
     >
       {children}
-      <span className="absolute inset-y-0 -z-10 w-screen bg-neutral-900 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100" />
+      <span className=" absolute inset-y-0 -z-10 w-screen bg-neutral-900 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100" />
     </Link>
   );
 };
 
 const Navigation = () => {
   return (
-    <nav className="mt-px font-display text-4xl font-medium tracking-tight text-white">
+    <nav className="mt-px font-display text-4xl font-medium tracking-tight text-white ">
       <NavigationRow>
         <NavigationItem href="/arifmetal">Arif Metal</NavigationItem>
         <NavigationItem href="/ariffabrications">Arif Fabrication Works</NavigationItem>
